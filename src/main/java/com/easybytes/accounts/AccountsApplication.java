7f0db0291a484e81df220e0952d28a5841e096e2
Joao Vitor <joaovitormoraveis@gmail.com>
2024-02-17 16:57:00 -0500

    Adding metadata for open api documentation

--- src/main/java/com/easybytes/accounts/AccountsApplication.java
+++ src/main/java/com/easybytes/accounts/AccountsApplication.java
@@ -1,10 +1,25 @@
 package com.easybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Account microservice REST API documentation",
+                description = "EasyBank Account microservice REST API documentation",
+                version = "v1",
+                contact = @Contact(name = "Joao Vitor", email = "joao@email.com", url = "localhost.com"),
+                license = @License(name = "Apache 2.0", url = "localhost.com")
+        ),
+        externalDocs = @ExternalDocumentation(description = "Other references", url = "other-references.com")
+)
 @SpringBootApplication
 public class AccountsApplication {
 
